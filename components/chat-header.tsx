9dc6ee8958f7290efdbe2690a1f74d2c366c2eb3
Agung Wirakurniawan <45479562+aggwrk@users.noreply.github.com>
2025-05-20 11:53:28 +0700

    Update chat-header.tsx

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -73,9 +73,9 @@ function PureChatHeader({
 
       <Button
         className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
-        
+        asChild
       >
-        <Link href={`https:/instagram.com/agung_wirak`} target="_noblank">
+        <Link href={`https:/instagram.com/agung_wirak`} target="_noblank" rel="noopener noreferrer">
           <VercelIcon size={16} />
           @agung_wirak
         </Link>
